a6f1dea1c2ff4a0bfd3daa916fb6caeaad9cbdf2
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-10-26 12:10:32 -0300

    Fixing send button

--- Chat app/client.js
+++ Chat app/client.js
@@ -5,18 +5,18 @@ const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('#messages-area');
 var audio = new Audio('pristine.mp3')
 
-const append = (message, position)=>{
+const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if(position == 'left'){
+    if (position == 'left') {
         audio.play();
     }
 }
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
     append(`You: ${message}`, 'right');
@@ -27,14 +27,14 @@ form.addEventListener('submit', (e)=>{
 const name = prompt("Enter your name to join the chat room");
 socket.emit('new-user-joined', name);
 
-socket.on('user-joined', name=>{
+socket.on('user-joined', name => {
     append(`${name} joined the chat`, 'left')
 })
 
-socket.on('receive', data=>{
+socket.on('receive', data => {
     append(`${data.name}: ${data.message}`, 'left')
 })
 
-socket.on('left', name=>{
+socket.on('left', name => {
     append(`${name} left the chat.`, 'left')
 })
